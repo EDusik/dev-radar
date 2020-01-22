e752ac3815689f18e964b8f8e8a9e485db105b67
eduardo_dusik <eduardo_dusik@terceiros.sicredi.com.br>
2020-01-21 22:04:28 -0300

    feat: Search and Dev Controller

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+// index, show, store, update and destroy
+
+module.exports = {
+
+    async index(request, response) {
+        const devs = await Dev.find();
+        return response.json(devs);
+    },
+
+    async store(request, response) {
+
+    const { github_username, techs, latitude, longitude } = request.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if(!dev) {
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        const { name = login, avatar_url, bio } = apiResponse.data;
+        const techsArray = parseStringAsArray(techs);
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+
+        dev = await Dev.create({
+            name,
+            github_username,
+            bio,
+            avatar_url,
+            techs: techsArray,
+            location
+        });
+    }
+
+        return response.json(dev);
+    }
+};
--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const PointSchema = require('./utils/PointSchema')
+const PointSchema = require('./utils/PointSchema');
 
 const DevSchema = new mongoose.Schema({
     name: String,
