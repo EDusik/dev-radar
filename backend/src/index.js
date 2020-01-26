57bc2bfbb1fd88fdea758806153609822b9b6f60
eduardo_dusik <eduardo_dusik@terceiros.sicredi.com.br>
2020-01-26 19:11:05 -0300

    feat: Accessing Back-end API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -17,6 +18,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-uqr4x.mongodb.net/w
     useCreateIndex: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from  './services/api';
 
 import './App.css';
 import './global.css';
@@ -6,6 +8,53 @@ import './Main.css'
 import './Sidebar.css'
 
 function App() {
+
+  const [devs, setDevs ] = useState([]);
+  const [github_username, setGithubUsername ] = useState('');
+  const [techs, setTechs ] = useState('');
+  const [latitude, setLatitude ] = useState('');
+  const [longitude, setLongitude ] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      });
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+    
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+    setDevs([...devs, response.data]);
+  } 
+
   return (
     <div id="app">
       <aside>
@@ -13,77 +62,40 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username">Github Username</label>
-            <input name="github_username" id="github_username" required />
+            <input name="github_username" id="github_username" value={github_username} onChange={e => setGithubUsername(e.target.value)} required />
           </div>
           <div className="input-block">
             <label htmlFor="techs">Technologies</label>
-            <input name="techs" id="techs" required />
+            <input name="techs" id="techs" value={techs} onChange={e => setTechs(e.target.value)} required />
           </div>
             <div className="input-group">
               <div className="input-block">
                 <label htmlFor="latitude">Latitude</label>
-                <input name="latitude" id="latitude" required />
+                <input type="number" name="latitude" id="latitude" value={latitude} onChange={e => setLatitude(e.target.value)} required />
               </div>
               <div className="input-block">
                 <label htmlFor="longitude">Longitude</label>
-                <input name="longitude" id="longitude" required />
+                <input type="number" name="longitude" id="longitude" value={longitude} onChange={e => setLongitude(e.target.value)} required />
               </div>
             </div>          
-          <button type="submit">Save</button>
+          <button type="submit" onClick={handleAddDev}>Save</button>
         </form>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/18349592?s=400&u=b34281200c17951b2d5da9c30aeb805349ed806e&v=4" alt="" />
-              <div className="user-info">
-                <strong>Eduardo Dusik</strong>
-                <span>Angular, React Js, Node Js</span>
-                <p>Lover of RPG, card games and programming. I really like power metal and i'm unable to stay more than 12 hours without coffee.</p>
-                <a href="https://github.com/edusik">access github profile</a>
-              </div>
-            </header>
-          </li>
-
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/18349592?s=400&u=b34281200c17951b2d5da9c30aeb805349ed806e&v=4" alt="" />
-              <div className="user-info">
-                <strong>Eduardo Dusik</strong>
-                <span>Angular, React Js, Node Js</span>
-                <p>Lover of RPG, card games and programming. I really like power metal and i'm unable to stay more than 12 hours without coffee.</p>
-                <a href="https://github.com/edusik">Access github profile</a>
-              </div>
-            </header>
-          </li>
-
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/18349592?s=400&u=b34281200c17951b2d5da9c30aeb805349ed806e&v=4" alt="" />
-              <div className="user-info">
-                <strong>Eduardo Dusik</strong>
-                <span>Angular, React Js, Node Js</span>
-                <p>Lover of RPG, card games and programming. I really like power metal and i'm unable to stay more than 12 hours without coffee.</p>
-                <a href="https://github.com/edusik">Access github profile</a>
-              </div>
-            </header>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/18349592?s=400&u=b34281200c17951b2d5da9c30aeb805349ed806e&v=4" alt="" />
-              <div className="user-info">
-                <strong>Eduardo Dusik</strong>
-                <span>Angular, React Js, Node Js</span>
-                <p>Lover of RPG, card games and programming. I really like power metal and i'm unable to stay more than 12 hours without coffee.</p>
-                <a href="https://github.com/edusik">Access github profile</a>
-              </div>
-            </header>
-          </li>
-
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+             <header>
+               <img src={dev.avatar_url} alt={dev.name} />
+               <div className="user-info">
+                 <strong>{dev.name}</strong>
+                 <span>{dev.techs.join(', ')}</span>
+                 <p>{dev.bio}</p>
+                 <a href={`https://github.com/${dev.github_username}`} target="_blank">access github profile</a>
+               </div>
+             </header>
+           </li>
+          ))}         
         </ul>
       </main>
     </div>
